e48db02cde482253666dd1ebde219733bd1ec0dd
unknown <delgadojuan222@gmail.com>
2019-05-04 12:48:59 -0500

    folder structure

---
+++ src/components/carousel/carousel.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+
+export default () => (
+  <Carousel>
+    <div>
+      
+    </div>
+  </Carousel>
+);
---
+++ src/components/modal/modal.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+
+const style = {
+  container: {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    background: "white",
+    border: "1px solid #ccc",
+    borderRadius: 8,
+    padding: 20,
+    maxWidth: 300,
+    transform: "translate(-50%, -50%)"
+  },
+  closeButton: {
+    fontSize: "2rem",
+    position: "absolute",
+    top: 10,
+    right: 10,
+    height: 10,
+    width: 10,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+};
+
+const Modal = props => (
+  <Fragment>
+    {props.opened ? (
+      <div style={style.container}>
+        <a onClick={props.onClose} href="#" style={style.closeButton}>
+          ×
+        </a>
+        {props.children}
+      </div>
+    ) : null}
+  </Fragment>
+);
+
+export default Modal;
