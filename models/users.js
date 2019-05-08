0ac2dc9a308f1f98c652ce0845b52febc95f2983
Antifreeze <preston.patterson1@gmail.com>
2019-05-07 20:55:21 -0500

    adding boilerplate

---
+++ models/users.js
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable indent */
+module.exports = function(sequelize, DataTypes) {
+    var Users = sequelize.define("Users", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 140],
+                is: /^[a-z]+$/i,
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+                
+            }
+        },
+        age: {
+            type: DataTypes.STRING,
+            // allowNull: false,
+            validate: {
+                isNumeric: true,
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        timestamps: false
+    });
+    return Users;
+};
