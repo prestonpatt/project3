0f3b8bccebc4141b50de42ff2e049388e253a6dd
unknown <delgadojuan222@gmail.com>
2019-05-09 20:19:08 -0500

    forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import './App.css';
-import Modal from "./components/modal/modal";
+import Nav from './components/Nav/Nav';
+import Modal from './components/Modal/Modal';
+import SignIn from './components/SignIn/SignIn';
+// import { Formik, Field, Form, ErrorMessage } from 'formik';
+// import * as Yup from 'yup';
 
-function App() {
-  return (
-    <div>
-      <Modal />
-    </div>
-  );
+// const TodoSchema = Yup.object().shape({
+//   message: Yup.string().required('Message is required.')
+// });
+
+class App extends React.Component {
+  state = {
+    modalOpened: true,
+    selectedTodo: null,
+  }
+
+  onModalClose = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => {
+      console.log(prevState);
+      return { modalOpened: !prevState.modalOpened };
+    });
+  }
+
+  onTodoClick = (id) => {
+    this.setState({
+      modalOpened: true,
+      selectedTodo: id,
+    })
+  }
+
+  onSubmit = (values) => {
+    alert(JSON.stringify(values));
+  }
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Modal opened={this.state.modalOpened} onClose={this.onModalClose}>
+          <SignIn />
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default App;
